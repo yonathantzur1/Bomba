43e02d43beb45cdebdebf0f135f5b3bb87782519
yonathantzur1 <yonathantzur1@gmail.com>
2020-02-14 17:10:20 +0200

    Add matrix side scroll cell follow

--- src/app/components/board/matrix/matrix.component.ts
+++ src/app/components/board/matrix/matrix.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cell } from './cell/cell.component';
 
 @Component({
@@ -8,7 +8,14 @@ import { Cell } from './cell/cell.component';
     styleUrls: ['./matrix.css']
 })
 
-export class MatrixComponent {
+export class MatrixComponent implements OnInit {
+    container: HTMLElement;
+    cellSize: number = 100;
+    minScrollCells: number;
+
+    ngOnInit(): void {
+        this.container = document.getElementById("matrix-container");
+    }
 
     matrix: Array<Array<Cell>> = [[new Cell()]]
 
@@ -16,9 +23,29 @@ export class MatrixComponent {
 
     addCol(i) {
         this.matrix[i].push(new Cell());
+        let currentColumnsAmount = this.matrix[i].length;
+
+        setTimeout(() => {
+            if (!this.minScrollCells) {
+                this.container.scrollLeft = this.container.scrollWidth - this.container.clientWidth;
+            }
+            else if (currentColumnsAmount >= this.minScrollCells) {
+                this.container.scrollLeft =
+                    (currentColumnsAmount - this.minScrollCells) * this.cellSize
+            }
+
+            if (!this.minScrollCells &&
+                (this.container.scrollWidth > this.container.clientWidth)) {
+                this.minScrollCells = currentColumnsAmount - 1;
+            }
+        }, 0);
     }
 
     addRow() {
         this.matrix.push([new Cell()]);
+
+        setTimeout(() => {
+            this.container.scrollTop = this.container.scrollHeight - this.container.clientHeight;
+        }, 0);
     }
 }
