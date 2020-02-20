353848ed437ee641596b938edaedd271da3896da
yonathantzur1 <yonathantzur1@gmail.com>
2020-02-20 19:44:41 +0200

    Update angular version and port from env variable

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 module.exports = {
     server: {
-        port: 8000,
+        port: process.env.PORT || 8000,
         maxRequestSize: '10mb'
     },
     db: {
         name: "bomba",
         connectionString: process.env.DEV_CONNECTION_STRING,
-        collections: {
-
-        }
+        collections: {}
     },
 }
