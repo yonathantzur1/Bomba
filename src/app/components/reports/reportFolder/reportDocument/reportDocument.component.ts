ce05e36b7c052bc203c382cf6b548900235e2152
yonathantzur1 <yonathantzur1@gmail.com>
2020-06-25 20:37:42 +0300

    Update to angular 10

--- src/app/components/reports/reportFolder/reportDocument/reportDocument.component.ts
+++ src/app/components/reports/reportFolder/reportDocument/reportDocument.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Document } from '../reportFolder.component'
 
-
 @Component({
     selector: 'report-document',
     templateUrl: './reportDocument.html',
